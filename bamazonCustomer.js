f21e36b96ecd4396f043df96aecdb1b620d14947
Anthony Sanchez <asanchez2194@gmail.com>
2019-12-02 20:20:48 -0500

    fixing undefined error

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,7 +17,7 @@ function queryAllProducts() {
     connection.query("SELECT * FROM products", function(err, res) {
       if (err) throw err;
       for (var i = 0; i < res.length; i++) {
-        console.log(res[i].id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+        console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
       }
       console.log("-----------------------------------");
     });
